ad51e88c8cd893638ae06244d0f71d71620e40a2
Eddy Zhu <67758739+Tronbo@users.noreply.github.com>
2021-03-26 12:44:47 -0400

    Add files via upload
    I really should have added a git ignore

---
+++ 4A-Watermelon/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _4A_Watermelon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int melonWeight = Convert.ToInt16(Console.ReadLine());
+
+            if(melonWeight >= 1 && melonWeight <= 100 && melonWeight % 2 == 0){
+                Console.WriteLine("True");
+            }else{
+            Console.WriteLine("False");
+            }   
+
+            Console.ReadKey();
+        }
+    }
+}
